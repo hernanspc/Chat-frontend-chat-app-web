f8e369ea626f07f6c7aa8ffd4a54966a82300c7a
Hernan Pormachi Quichca <hernan.pormachi.quichca@everis.nttdata.com>
2022-07-24 10:47:23 -0500

    conectar y desconectar socket

--- src/context/SocketContext.js
+++ src/context/SocketContext.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { createContext } from 'react';
+import { AuthContext } from '../auth/AuthContext';
 import { useSocket } from '../hooks/useSocket'
 
 export const SocketContext = createContext();
@@ -7,7 +8,23 @@ export const SocketContext = createContext();
 
 export const SocketProvider = ({ children }) => {
 
-    const { socket, online } = useSocket('http://localhost:8080');
+    const { socket, online, conectarSocket, desconectarSocket } = useSocket('http://localhost:8080');
+    const { auth } = useContext(AuthContext)
+
+    useEffect(() => {
+        if (auth.logged) {
+            conectarSocket();
+        }
+
+    }, [auth, conectarSocket])
+
+    useEffect(() => {
+        if (!auth.logged) {
+            desconectarSocket();
+        }
+
+    }, [auth, desconectarSocket])
+
 
     return (
         <SocketContext.Provider value={{ socket, online }}>

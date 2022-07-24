082ddc5d82837e151a3308ddcac383686f37fabe
Hernan Pormachi Quichca <hernan.pormachi.quichca@everis.nttdata.com>
2022-07-24 03:52:43 -0500

    estableciendo rutas estaticas publicas y privadas

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -10,6 +10,8 @@ import { AuthContext } from "../auth/AuthContext";
 
 import ChatPage from '../pages/ChatPage';
 import { AuthRouter } from "./AuthRouter";
+import PrivateRoute from "./PrivateRoute";
+import PublicRoute from "./PublicRoute";
 
 export const AppRouter = () => {
 
@@ -19,16 +21,17 @@ export const AppRouter = () => {
         verificaToken();
     }, [verificaToken])
 
-    if (auth.checking) {
-        return <h1>Espere por favor</h1>
-    }
+    // if (auth.checking) {
+    //     return <h1>Espere por favor</h1>
+    // }
 
     return (
         <Router>
             <div>
                 <Switch>
-                    <Route path='/auth' component={AuthRouter} />
-                    <Route exact path='/' component={ChatPage} />
+                    {/* <Route path='/auth' component={AuthRouter} /> */}
+                    <PublicRoute isAuthenticated={auth.logged} path='/auth' component={AuthRouter} />
+                    <PrivateRoute isAuthenticated={auth.logged} exact path='/' component={ChatPage} />
                     <Redirect to='/' />
                 </Switch>
             </div>
